cd98c8630009f9ceeb5928eb4b27aa7e631d29e1
orangeappleak <appannagarikarthik.123@gmail.com>
2021-06-09 19:46:21 +0530

    tested some minor changes for firebase

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,7 +5,7 @@ import ui from './firebase';
 import {useEffect,useState} from 'react';
 
 function App() {
-
+  
   const [loggedin, updateLogin] = useState(false);
   const [profile, updateProfile] = useState({
     profile_name: "",
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="main-page">
-      <div id="login-page">
+      <div className = "animate" id="login-page">
         <div id="main-image">
           <img alt=" " src="https://images.unsplash.com/photo-1601469090980-fc95e8d95544?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE4fHxib29rc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"/>
         </div>
@@ -25,27 +25,42 @@ function App() {
           <h1>Hello There.</h1>
           
           <p>Welcome to a new way of reading and trading books, Welcome to the<span id="imp"> Book Exchange Club.</span> </p>
-          {loggedin ?<p>Logged in as: <span id="imp">{profile.profile_name}</span></p> :<div id="login-panel">
-            {ui.start('#login-panel',{
-              callbacks: {
-                signInSuccessWithAuthResult: function(authResult, redirectUrl){
-                  updateLogin(true);
-                  updateProfile({
-                    profile_name: authResult.additionalUserInfo.profile.name
-                  });
-                  return false;
-                }
-              },
-              signInOptions: [
-                firebase.auth.GoogleAuthProvider.PROVIDER_ID
-              ],
-              signInFlow: 'popup'
-            })}
-          </div> }
+
+          {loggedin ? animatePage() : < LoginPanel updateLogin = {updateLogin} updateProfile = {updateProfile}/>}
         </div>
       </div>
     </div>
   );
+}
+
+
+function animatePage(){
+  var animate = document.getElementsByClassName("animate");
+  [...animate].forEach(ele => {
+    ele.classList.add("loginSuccess")
+  });
+}
+
+function LoginPanel({updateLogin,updateProfile}){
+  return(
+    <div id="login-panel">
+      {ui.start("#login-panel",{
+        callbacks: {
+          signInSuccessWithAuthResult(authuser, redirecturl){
+            updateLogin(true)
+            updateProfile({profile_name: authuser.additionalUserInfo.profile.name})
+            return false
+          }
+        },
+        signInOptions: [
+          {
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID
+          }
+        ],
+        signInFlow: 'popup'
+      })}
+    </div>
+  )
 }
 
 export default App;
