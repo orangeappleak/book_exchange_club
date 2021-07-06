927f6d2c76c765e283197e7594518ae9f41385a4
orangeappleak <appannagarikarthik.123@gmail.com>
2021-07-06 19:23:08 +0530

    minor changes

--- src/Components/TopBooks.js
+++ src/Components/TopBooks.js
@@ -25,7 +25,7 @@ export default function TopCategories(){
         console.log(JSON.parse(local_books_data));
 
         if(local_books_data === null){
-            fetch('/api/popularBooks')
+            fetch("/api/popularBooks")
             .then(data => {
                 console.log("data",data.json());
 
